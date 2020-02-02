f2caac7b99876dd278b282210b6755fb460ed8e7
jinorgin <JinKwan.Kim@georgebrown.ca>
2020-02-01 19:54:00 -0500

    not yet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { UserSignupComponent } from "./component/user-signup/user-signup.component";
 import { UserDashboardComponent } from "./component/user-dashboard/user-dashboard.component";
+import { UserHistoryComponent } from './component/user-history/user-history.component';
+import { UserCommentComponent } from './component/user-comment/user-comment.component';
 
 @NgModule({
-  declarations: [AppComponent, UserSignupComponent, UserDashboardComponent],
+  declarations: [AppComponent, UserSignupComponent, UserDashboardComponent, UserHistoryComponent, UserCommentComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
